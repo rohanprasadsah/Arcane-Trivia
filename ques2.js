b1abfb53abb0af5fc578c81273aa52612ff1db99
rohanprasadsah <rohanprasadsah70@gmail.com>
2024-11-07 21:55:24 +0530

    First commit with HTML, CSS, and JavaScript files

---
+++ ques2.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        num: 1,
+        question: "Which is the most powerful form of Son Goku yet?",
+        answer: "B. Mastered Ultra Instinct",
+        options: [
+            "A. Super Saiyan",
+            "B. Mastered Ultra Instinct",
+            "C. Super Saiyan Blue",
+            "D. Ultra Instinct"
+        ]
+    },
+    {
+        num: 2,
+        question: "Which is Satoru Gojo's most powerfull attack in Jujutsu Kaisen?",
+        answer: "A. Hollow Technique: Purple",
+        options: [
+            "A. Hollow Technique: Purple",
+            "B. Domain Expansion: Infinite Void",
+            "C. Limitless",
+            "D. Cursed Technique Reversal: Red"
+        ]
+    },
+    {
+        num: 3,
+        question: "What are the breathing styles Tanjiro Kamado uses in Demon Slayer?",
+        answer: "C. Water Breathing and Sun Breathing",
+        options: [
+            "A. Water Breathing and Thunder Breathing",
+            "B. Thunder Breathing and Flame Breathing",
+            "C. Water Breathing and Sun Breathing",
+            "D. Wind Breathing and Water Breathing"
+        ]
+    },
+    {
+        num: 4,
+        question: "What transformation does Ash's Greninja achieve in Pokémon?",
+        answer: "D. Ash-Greninja Form",
+        options: [
+            "A. Mega Evolution",
+            "B. Dynamax",
+            "C. Bond Evolution",
+            "D. Ash-Greninja Form"
+        ]
+    },
+    {
+        num: 5,
+        question: "What is Muzan Kibutsuji's primary ability in Demon Slayer?",
+        answer: "A. Blood Demon Art",
+        options: [
+            "A. Blood Demon Art",
+            "B. Spirit Gun",
+            "C. Rasengan",
+            "D. Sharingan"
+        ]
+    }
+]
---
+++ ques4.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        num: 1,
+        question: "What is the name of our galaxy?",
+        answer: "B. Milky Way",
+        options: [
+            "A. Andromeda",
+            "B. Milky Way",
+            "C. Whirlpool",
+            "D. Sombrero"
+        ]
+    },
+    {
+        num: 2,
+        question: "What is the most accepted theory for the origin of the universe?",
+        answer: "B. Big Bang Theory",
+        options: [
+            "A. Steady State Theory",
+            "B. Big Bang Theory",
+            "C. Oscillating Universe Theory",
+            "D. Multiverse Theory"
+        ]
+    },
+    {
+        num: 3,
+        question: "What is a supermassive black hole at the center of the Milky Way galaxy called?",
+        answer: "C. Sagittarius A*",
+        options: [
+            "A. Cygnus X-1",
+            "B. Andromeda Black Hole",
+            "C. Sagittarius A*",
+            "D. Messier 87"
+        ]
+    },
+    {
+        num: 4,
+        question: "What is the term for the point in space where the gravitational pull is so strong that not even light can escape?",
+        answer: "D. Black Hole",
+        options: [
+            "A. White Dwarf",
+            "B. Neutron Star",
+            "C. Event Horizon",
+            "D. Black Hole"
+        ]
+    },
+    {
+        num: 5,
+        question: "What is the name of the largest known structure in the universe?",
+        answer: "C. Hercules-Corona Borealis Great Wall",
+        options: [
+            "A. Local Group",
+            "B. Sloan Great Wall",
+            "C. Hercules-Corona Borealis Great Wall",
+            "D. Virgo Supercluster"
+        ]
+    }
+]
